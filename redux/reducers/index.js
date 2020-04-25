945e893b761805499873c483933e9e4175e8ac19
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-04-25 16:07:48 +0530

    completed reducers

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,10 +1,25 @@
+import { ADD_INGREDIENTS, DELETE_INGREDIENTS } from "../constants/actionTypes";
+
 const initialState = {
   ingredients: [],
   recipie: "Fruitsalads",
 };
 
 function rootReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case ADD_INGREDIENTS:
+      return Object.assign({}, state, {
+        ingredients: state.ingredients.concat(action.text),
+      });
+    case DELETE_INGREDIENTS:
+      return Object.assign({}, state, {
+        ingredients: state.ingredients
+          .slice(0, action.index)
+          .concat(state.ingredients.slice(action.index + 1)),
+      });
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
